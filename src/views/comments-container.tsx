fc46fba0ed049d6698d59162abac6b3245eb9fa7
jjxmonster <kuba.tarabasz@wp.pl>
2024-03-17 18:09:30 +0100

    Remove Hydration Boundary since it's unnecessary

--- src/views/comments-container.tsx
+++ src/views/comments-container.tsx
@@ -1,9 +1,5 @@
 import { getCommentsByPostID } from "@/actions/comments";
-import {
-	HydrationBoundary,
-	QueryClient,
-	dehydrate,
-} from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { CommentsList } from "./comments-list";
 import { AddCommentForm } from "@/components/add-comment-form";
 import { getServerSession } from "next-auth";
@@ -21,9 +17,7 @@ export const CommentsContainer = async ({ id }: { id: number }) => {
 	return (
 		<div className="mt-20 border-t pt-5">
 			<h2 className="mb-5 text-2xl font-bold">Comments</h2>
-			<HydrationBoundary state={dehydrate(queryClient)}>
-				<CommentsList id={id} />
-			</HydrationBoundary>
+			<CommentsList id={id} />
 			{session && <AddCommentForm post_id={id} />}
 		</div>
 	);
