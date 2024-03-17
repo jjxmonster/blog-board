62c59bdc1c8cdc34fa579027960116a69f6453d9
jjxmonster <kuba.tarabasz@wp.pl>
2024-03-17 18:02:36 +0100

    Implemented loading for pages change

--- src/components/ui/loading-indicator.tsx
+++ src/components/ui/loading-indicator.tsx
@@ -1,7 +1,7 @@
 import { Loader2 } from "lucide-react";
 export const LoadingIndicator = () => {
 	return (
-		<div className="flex w-full items-center justify-center gap-2 py-32">
+		<div className="flex h-full w-full items-center justify-center gap-2 py-32">
 			<Loader2 size={15} className="animate-spin text-gray-400" />{" "}
 			<span className="text-sm text-gray-400"> Loading...</span>
 		</div>
--- src/views/comments-list.tsx
+++ src/views/comments-list.tsx
@@ -4,11 +4,7 @@ import { CommentCard } from "@/components/comment-card";
 import { useGetCommentsByPostID } from "@/data/get-comments";
 
 export const CommentsList = ({ id }: { id: number }) => {
-	const { data, isLoading } = useGetCommentsByPostID(id);
-
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
+	const { data } = useGetCommentsByPostID(id);
 
 	return (
 		<div className="flex flex-col space-y-5">
